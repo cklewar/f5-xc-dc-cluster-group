e1da48f2a5ce06853b4aef3fa9eaf8ddc09d315a
cklewar@t-online.de <cklewar@t-online.de>
2022-10-06 19:27:44 +0200

    0.11.14 v1

--- versions.tf
+++ versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.2.7"
+  required_version = ">= 1.3.0"
 
   cloud {
     organization = "cklewar"
@@ -13,7 +13,7 @@ terraform {
   required_providers {
     volterra = {
       source  = "volterraedge/volterra"
-      version = ">= 0.11.13"
+      version = ">= 0.11.14"
     }
 
     local = ">= 2.2.3"
