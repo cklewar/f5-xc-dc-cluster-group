8bbb4f33746d4120b7efd7c0b4b989973ebe5654
cklewar <cklewar@t-online.de>
2023-01-20 11:39:08 +0100

    0.11.18

--- versions.tf
+++ versions.tf
@@ -1,22 +1,20 @@
 terraform {
   required_version = ">= 1.3.0"
-
   cloud {
-    organization = "cklewar"
+    organization = cklewar"
     hostname     = "app.terraform.io"
 
     workspaces {
-      name = "f5-xc-dc-cluster-group-module"
+      release = ""
     }
   }
-
+  
   required_providers {
     volterra = {
-      source  = "volterraedge/volterra"
-      version = "= 0.11.16"
+      source = "volterraedge/volterra"
+      version = "= 0.11.18"
     }
-
     local = ">= 2.2.3"
-    null  = ">= 3.1.1"
+    null = ">= 3.1.1"
   }
 }
